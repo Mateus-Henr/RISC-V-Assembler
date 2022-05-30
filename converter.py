db564c1b18769d1b87289b30f6b518c930f7953e
Mateus-Henr <mateush.study@gmail.com>
2022-05-30 12:55:25 -0300

    Organise code, and add overflow functionality

--- converter.py
+++ converter.py
@@ -3,92 +3,132 @@ from types import RType, IType, SType, UType
 FUNCT3 = 0
 FUNCT7 = 1
 
+R_TYPES = {
+    "add": ["000", "0000000"],
+    "sub": ["000", "0100000"],
+    "and": ["111", "0000000"],
+    "or": ["110", "0000000"],
+    "xor": ["100", "0000000"],
+    "sll": ["001", "0000000"],
+    "srl": ["101", "0000000"],
+    "lrd": ["011", "0010000"],
+    "scd": ["011", "0001100"],
+
+    # Pseudo instructions
+    "neg": ["000", "0100000"]
+}
+
+I_TYPES = {
+    "addi": ["000"],
+    "andi": ["111"],
+    "ori": ["110"],
+    "lb": ["000"],
+    "lbu": ["100"],
+    "lh": ["001"],
+    "lhu": ["101"],
+    "lw": ["010"],
+    "lwu": ["110"],
+    "ld": ["011"],
+
+    # Pseudo instructions
+    "nop": ["000"],
+    "li": ["000"],
+    "mv": ["000"]
+}
+
+S_TYPES = {
+    "sd": ["111"],
+    "sw": ["010"],
+    "sh": ["001"],
+    "sb": ["000"]
+}
+
+U_TYPES = {"lui"}
+
 
 def build_instruction(line: str):
     """"""
 
-    r_types = {
-        "add": ["000", "0000000"],
-        "sub": ["000", "0100000"],
-        "and": ["111", "0000000"],
-        "or": ["110", "0000000"],
-        "xor": ["100", "0000000"],
-        "sll": ["001", "0000000"],
-        "srl": ["101", "0000000"],
-        "lrd": ["011", "0010000"],
-        "scd": ["011", "0001100"],
+    instruction_name = line.split()[0]
 
-        # Pseudo instructions
-        "neg": ["000", "0100000"]
-    }
+    if instruction_name in R_TYPES:
+        user_input = line.replace(",", " ").split()
 
-    i_types = {
-        "addi": ["000"],
-        "andi": ["111"],
-        "ori": ["110"],
-        "lb": ["000"],
-        "lbu": ["100"],
-        "lh": ["001"],
-        "lhu": ["101"],
-        "lw": ["010"],
-        "lwu": ["110"],
-        "ld": ["011"],
+        instruction = {
+            "funct7": R_TYPES[user_input[0]][FUNCT7],
+            "rs2": get_register_binary_code(user_input[3]),
+            "rs1": get_register_binary_code(user_input[2]),
+            "funct3": R_TYPES[user_input[0]][FUNCT3],
+            "rd": get_register_binary_code(user_input[1])
+        }
 
-        # Pseudo instructions
-        "nop": ["000"],
-        "li": ["000"],
-        "mv": ["000"]
-    }
+        return RType(instruction["funct7"],
+                     instruction["rs2"],
+                     instruction["rs1"],
+                     instruction["funct3"],
+                     instruction["rd"])
 
-    s_types = {
-        "sd": ["111"],
-        "sw": ["010"],
-        "sh": ["001"],
-        "sb": ["000"]
-    }
+    elif instruction_name in I_TYPES:
+        user_input = line.replace(",", " ").split()
 
-    u_types = {"lui"}
+        instruction = {
+            "immediate": get_immediate_binary_12bits(user_input[3]),
+            "rs1": get_register_binary_code(user_input[2]),
+            "funct3": get_register_binary_code(user_input[1]),
+            "rd": I_TYPES[instruction_name][FUNCT3]
+        }
 
-    instruction = line.replace(",", " ").replace("(", " ").replace("-", " ").replace(")", " ").split()
-    instruction_name = instruction[0]
+        return IType(instruction["immediate"],
+                     instruction["rs1"],
+                     instruction["funct3"],
+                     instruction["rd"])
 
-    if instruction_name in r_types:
-        return RType(r_types[instruction_name][FUNCT7],
-                     get_register_binary_code(instruction[3]),
-                     get_register_binary_code(instruction[2]),
-                     r_types[instruction_name][FUNCT3],
-                     get_register_binary_code(instruction[1]))
+    elif instruction_name in S_TYPES:
+        user_input = line.replace(",", " ").replace("(", " ").replace(")", " ").split()
 
-    elif instruction_name in i_types:
-        return IType(get_immediate_binary_12bits(instruction[3]),
-                     get_register_binary_code(instruction[2]),
-                     get_register_binary_code(instruction[1]),
-                     i_types[instruction_name][FUNCT3])
+        immediate = get_immediate_binary_12bits(user_input[2])
 
-    elif instruction_name in s_types:
-        immediate = get_immediate_binary_12bits(instruction[2])
+        instruction = {
+            "immediate7": immediate[5:11],
+            "rs2": get_register_binary_code(user_input[3]),
+            "rs1": get_register_binary_code(user_input[1]),
+            "funct3": S_TYPES[instruction_name][FUNCT3],
+            "immediate5": immediate[0:4]
+        }
 
-        return SType(immediate[5:11],
-                     get_register_binary_code(instruction[3]),
-                     get_register_binary_code(instruction[1]),
-                     s_types[instruction_name][FUNCT3],
-                     immediate[0:4])
+        return SType(instruction["immediate7"],
+                     instruction["rs2"],
+                     instruction["rs1"],
+                     instruction["funct3"],
+                     instruction["immediate5"])
 
-    elif instruction_name in u_types:
-        return UType(get_immediate_binary_20bits(instruction[2]),
-                     get_register_binary_code(instruction[1]))
+    elif instruction_name in U_TYPES:
+        user_input = line.replace(",", " ").replace("(", " ").replace(")", " ").split()
+
+        instruction = {
+            "immediate": get_immediate_binary_20bits(user_input[2]),
+            "rd": get_register_binary_code(user_input[1])
+        }
+
+        return UType(instruction["immediate"],
+                     instruction["rd"])
 
     else:
         print(f"ERROR: Instruction name '{instruction_name}' not in the instruction set.")
 
 
 def get_register_binary_code(register):
-    return "{0:05b}".format(int(register.replace("x", "")))
+    return "{0:05b}".format(perform_overflow_if_happens(int(register.replace("x", "")), 5))
 
 
 def get_immediate_binary_12bits(immediate):
-    return "{0:012b}".format(int(immediate))
+    return "{0:012b}".format(perform_overflow_if_happens(int(immediate), 12))
 
 
 def get_immediate_binary_20bits(immediate):
-    return "{0:020b}".format(int(immediate))
+    return "{0:020b}".format(perform_overflow_if_happens(int(immediate), 20))
+
+
+def perform_overflow_if_happens(value, max_bits):
+    max_value = 2 ** max_bits
+    return value % max_value if value >= max_value else value

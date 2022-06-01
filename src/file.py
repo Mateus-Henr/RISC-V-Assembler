2789e45b261c4c1d0a0c77fac72f7248f028c70c
Mateus Henrique <mateush.study@gmail.com>
2022-06-01 16:57:40 -0300

    Fix command line arguments support

---
+++ src/file.py
@@ -0,0 +1,38 @@
+from converter import *
+
+INPUT_FILE_PATH = "../input_files/"
+OUTPUT_FILE_PATH = "../output_files/"
+
+
+def read_file_and_generate_output(input_filename: str, output_filename: str):
+    input_file = open(f"{INPUT_FILE_PATH}{input_filename}", "r")
+    output_file = open(f"{OUTPUT_FILE_PATH}{output_filename}.bin", "w+")
+
+    with output_file:
+        with input_file:
+            for line in input_file:
+                if pre_check_line(line):
+                    output_file.write(f"{assemble_instruction(line)}\n")
+
+    print(f"Output file generated at: {INPUT_FILE_PATH}{output_filename}.bin")
+
+
+def read_file_and_print(input_filename: str):
+    input_file = open(f"{INPUT_FILE_PATH}{input_filename}", "r")
+
+    with input_file:
+        for line in input_file:
+            if pre_check_line(line):
+                print(f"{assemble_instruction(line)}")
+
+
+# function that check if the line is readable and is not a comment line or an empty line
+def pre_check_line(line_to_check: str):
+    """receive a line to check if the line is readable, strip the line a check for empty spaces or comments.
+
+    input: a line string to check.
+
+    output: a string that is readable."""
+
+    line_to_check = line_to_check.strip()
+    return line_to_check not in ['\n', '\r\n'] and len(line_to_check) > 0 and line_to_check[0] != "#"
--- src/main.py
+++ src/main.py
@@ -1,67 +1,15 @@
 """Main code of the project, open the selected file and return the machine binary code of it."""
 
-# internal imports
-from converter import *
 import sys
+from file import *
 
-# stores the path file of the tests
-INPUT_FILE_PATH = "input_files/"
-OUTPUT_FILE_PATH = "output_files/"
-
-
-# function that check if the line is readable and is not a comment line or an empty line
-def pre_check_line(line_to_check: str):
-    """receive a line to check if the line is readable, strip the line a check for empty spaces or comments.
-
-    input: a line string to check.
-
-    output: a string that is readable."""
-
-    line_to_check = line_to_check.strip()
-    return line_to_check not in ['\n', '\r\n'] and len(line_to_check) > 0 and line_to_check[0] != "#"
-
-
-filename = ""
-
-if len(sys.argv) <= 3:
-    filename = input("Filename: ")
-else:
-    filename = sys.argv[3]
-
-# try to open the input file if it exists else returns an error message
-try:
-    input_file = open(f"{INPUT_FILE_PATH}{filename}", "r")
-except FileNotFoundError:
-    raise FileNotFoundError(f"Error reading the file:{filename}")
-
-if len(sys.argv) > 0:
-    with input_file:
-        for line in input_file:
-            if pre_check_line(line):
-                print(f"{assemble_instruction(line)}")
-    sys.exit()
-
-# try to create the output file if it can't return error message
-try:
-    output_file = open(f"{OUTPUT_FILE_PATH}{filename.split('.')[0]}-output.bin", "w+")
-except OSError:
-    raise OSError("ERROR: Could not create the output file")
-
-    # read each input file and use the build instruction to create a binary code for each instruction, and store it in the
-    # output file
-
-with output_file:
-    with input_file:
-        for line in input_file:
-            if pre_check_line(line):
-                output_file.write(f"{assemble_instruction(line)}\n")
-
-
-# function that gets the file name without extension
-def get_filename_without_extension(filename_to_format: str):
-    """check is an . in the file that indicates an extension and return a string without it.
-
-    input: a string with the file name
-    output: a string with the file name without extension"""
-
-    return filename_to_format.split(".") if "." in filename_to_format else filename_to_format
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        read_file_and_print(sys.argv[1])
+    elif len(sys.argv) == 6:
+        if sys.argv[4] == "-o":
+            read_file_and_generate_output(sys.argv[3], sys.argv[5])
+        else:
+            raise ValueError("ERROR: Missing arguments.")
+    else:
+        raise ValueError("ERROR: Missing arguments or too many arguments.")

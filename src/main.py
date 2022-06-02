4d296bb71796dfe3634a905a5585b44370c82b6c
Mateus-Henr <mateush.study@gmail.com>
2022-06-02 20:00:43 -0300

    Add comment that describes the possibilities of number of parameters from command line arguments

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         read_file_and_print(sys.argv[1])
     # When using PyCharm we can set in the parameters box only "input_file -o output_file" (i.e. "input.asm -o output")
+    # Because of that, more the path that is automatically added, we have four parameters. Another possibility is when
+    # we use the command line, so it's usually 5 parameters.
     elif len(sys.argv) == 4 or len(sys.argv) == 5:
         if sys.argv[2] == "-o":
             read_file_and_generate_output(sys.argv[1], sys.argv[3])
